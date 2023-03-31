af274a5262d95e0c2fbcb4acbede6072a6c91e4e
Pavlomatolikov <Pavlomatolikov@gmail.com>
2023-03-31 14:25:01 +0200

    Basket button test was refactored

--- localization_test/test_items.py
+++ localization_test/test_items.py
@@ -1,14 +1,12 @@
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
-import pytest
-import time
 
 
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
+
 def test_add_to_basket_button_is_present(browser):
     browser.get(link)
-    add_to_basket_button = None
     try:
         add_to_basket_button = browser.find_element(By.CSS_SELECTOR, "button.btn-add-to-basket")
         is_button_visible = add_to_basket_button.is_displayed()
